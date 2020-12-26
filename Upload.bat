8bb15265411ba3eb66bee0f25395efa5d9088b3e
Pikachuim <pikachuim@qq.com>
2020-12-27 04:18:40 +0800

    Updated on a nice day

--- Upload.bat
+++ Upload.bat
@@ -26,5 +26,6 @@ echo             ¨~                                ¨~
 echo             ¨~¨~¨~¨~¨~¨~¨~¨~¨~¨~¨~¨~¨~¨~¨~¨~¨~¨~
 git add .
 git commit -m "Updated"%date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%
+git commit -m "Updated on a nice day"
 git push
 timeout /t 5
